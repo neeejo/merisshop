4efaa126876e3142b2173c3f1ea04a017aa5cd29
NChironna <n.chironna@contrader.it>
2023-11-25 15:21:07 +0100

    Controller->RestController

--- merisshop/src/main/java/org/example/merisshop/controller/UserController.java
+++ merisshop/src/main/java/org/example/merisshop/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
